e29a07c8d533c0a320ea3ed778c8756b5083b872
Ebele-Abolo <77888430+Ebele-Abolo@users.noreply.github.com>
2023-07-20 14:44:34 -0500

    Add files via upload

---
+++ 200.py
@@ -0,0 +1,3 @@
+function_Widget = widgets.Dropdown(opentions=funcionality)
+new_df = df.sort_values(column, ascending=True)
+variable_name = widgets.interactive(function_name,dropdown1=value, dropdown2=value)
